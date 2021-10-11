fa2fc18b90cfecaa4540013177797c3def118c52
Evgeny Savelov <igluko@gmail.com>
2021-10-11 09:34:06 +0300

    fix bug - wrong statement when 0 vm is running

--- pve-meminfo.sh
+++ pve-meminfo.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 function getServerStats(){
-/usr/sbin/qm list | grep running | awk '{print $4}' | paste -sd+ | awk '{printf "scale=1;(%s)/1024\n", ($1) }' | bc | awk '{print "#Running VM Reserved: " $1 " GB"}'
+running_vm=`/usr/sbin/qm list | grep running`
+if [ -z "$running_vm" ]; then
+  echo "#Running VM Reserved: 0 GB"
+else
+  echo "$running_vm" | awk '{print $4}' | paste -sd+ | awk '{printf "scale=1;(%s)/1024\n", ($1) }' | bc | awk '{print "#Running VM Reserved: " $1 " GB"}'
+fi
 echo "#"
 awk '{printf "#%s  %.0f GB \n" , $1 , $2=$2/1024^2}' /proc/meminfo | grep -v "0"
 echo "#"
