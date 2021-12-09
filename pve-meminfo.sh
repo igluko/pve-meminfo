1f0ec4412d3b9536c0512e841836ab349f9c5599
Darya Savelova <kuznecova.d.m@gmail.com>
2021-12-10 02:43:53 +0300

    possible free mem and formatting

--- pve-meminfo.sh
+++ pve-meminfo.sh
@@ -2,15 +2,22 @@
 
 function getServerStats(){
 running_vm=`/usr/sbin/qm list | grep running`
-if [ -z "$running_vm" ]; then
-  echo "#Running VM Reserved: 0 GB"
-else
-  echo "$running_vm" | awk '{print $4}' | paste -sd+ | awk '{printf "scale=1;(%s)/1024\n", ($1) }' | bc | awk '{print "#Running VM Reserved: " $1 " GB"}'
+running_mem=0
+if [ -n "$running_vm" ]; then
+  running_mem=`echo "$running_vm" | awk '{print $4}' | paste -sd+ | awk '{printf "scale=0;(%s)/1024\n", ($1) }' | bc`
 fi
+
+free_mem=`cat /proc/meminfo | awk '$1=="MemFree:" {printf "%.f", $2/1024/1024 }'`
+arc_mem=`cat /proc/spl/kstat/zfs/arcstats | awk '$1=="size" {printf "%.f", $3/1024/1024/1024 }'`
+possible_free_mem=$(( $free_mem + $arc_mem ))
+
+echo "#Possible Free mem: **$possible_free_mem GB**  "
 echo "#"
-awk '{printf "#%s  %.0f GB \n" , $1 , $2=$2/1024^2}' /proc/meminfo | grep -v "0"
+echo "#Running VM Reserved: **$running_mem GB**  "
 echo "#"
-awk '$1=="size" {printf "#ARC SIZE = %.f GB \n", $3/1024/1024/1024 }' /proc/spl/kstat/zfs/arcstats
+awk '{printf "#%s  %.0f GB  \n" , $1 , $2=$2/1024^2}' /proc/meminfo | grep -v "0"
+echo "#"
+awk '$1=="size" {printf "#ARC SIZE = **%.f GB** \n", $3/1024/1024/1024 }' /proc/spl/kstat/zfs/arcstats
 echo "#"
 }
 getServerStats>"/etc/pve/local/config"
